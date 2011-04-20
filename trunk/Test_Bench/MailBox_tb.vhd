580c146b10fcb69d169f31d61f7bfdef8aa7cf1b
Julien1138 <Julien1138@6bd332d3-07b6-4db3-bbff-38d17ce5034a>
2011-04-20 15:48:18 +0000

    Réintégration des sous-blocs dans le Top-Level

--- trunk/Test_Bench/MailBox_tb.vhd
+++ trunk/Test_Bench/MailBox_tb.vhd
@@ -23,203 +23,217 @@ entity MailBox_tb is
    generic
    (
       g_RTCClockPeriode    : std_logic_vector := "100";
-      WB_Addr_Width         : integer := 4;
-      WB_Data_Width         : integer := 32;
-      RTC_time_Width        : integer := 16
+      g_WB_Addr_Width         : integer := 4;
+      g_WB_Data_Width         : integer := 32;
+      g_RTC_time_Width        : integer := 16
    );
 end MailBox_tb;
 
 architecture behavior of MailBox_tb is
    
-   signal wb_clk_i      : std_logic := '1';
-   signal wb_rst_i      : std_logic := '1';
+   signal s_clk   : std_logic := '1';
+   signal s_rst   : std_logic := '1';
    
 -- Settings and Data Read Interface
-   signal wb_we_i_User   : std_logic := '0';
-   signal wb_adr_i_User  : std_logic_vector(WB_Addr_Width + 2 downto 0) := (others => '0');
-   signal wb_dat_i_User  : std_logic_vector(WB_Data_Width - 1 downto 0) := (others => '0');
-   signal wb_dat_o_User  : std_logic_vector(WB_Data_Width - 1 downto 0);
-   signal wb_cyc_i_User  : std_logic := '0';
-   signal wb_stb_i_User  : std_logic := '0';
-   signal wb_ack_o_User  : std_logic;
-   signal wb_dtr_o_User  : std_logic;   -- Data is available to be read
-   signal wb_atr_o_User  : std_logic_vector(WB_Addr_Width downto 0); -- Address at which Data should be read
+   signal s_wb_we_UserToUut   : std_logic := '0';
+   signal s_wb_adr_UserToUut  : std_logic_vector(g_WB_Addr_Width + 2 downto 0) := (others => '0');
+   signal s_wb_dat_UserToUut  : std_logic_vector(g_WB_Data_Width - 1 downto 0) := (others => '0');
+   signal s_wb_dat_UutToUser  : std_logic_vector(g_WB_Data_Width - 1 downto 0);
+   signal s_wb_cyc_UserToUut  : std_logic := '0';
+   signal s_wb_stb_UserToUut  : std_logic := '0';
+   signal s_wb_ack_UutToUser  : std_logic;
+   signal s_DataAvailable     : std_logic;   -- Data is available to be read
+   signal s_AddrToRead        : std_logic_vector(g_WB_Addr_Width downto 0); -- Address at which Data should be read
    
 -- External Master Interface
-   signal wb_we_o_Master  : std_logic;
-   signal wb_adr_o_Master : std_logic_vector(WB_Addr_Width - 1 downto 0);
-   signal wb_dat_o_Master : std_logic_vector(WB_Data_Width - 1 downto 0);
-   signal wb_dat_i_Master : std_logic_vector(WB_Data_Width - 1 downto 0) := X"87654321";
-   signal wb_cyc_o_Master : std_logic;
-   signal wb_stb_o_Master : std_logic;
-   signal wb_ack_i_Master : std_logic;
-   signal wb_ack_i_Master_int : std_logic;
-         
-   constant clk_period : time := 20 ns;   -- 50 MHz
+   signal s_wb_we_UutToSlave  : std_logic;
+   signal s_wb_adr_UutToSlave : std_logic_vector(g_WB_Addr_Width - 1 downto 0);
+   signal s_wb_dat_UutToSlave : std_logic_vector(g_WB_Data_Width - 1 downto 0);
+   signal s_wb_dat_SlaveToUut : std_logic_vector(g_WB_Data_Width - 1 downto 0) := X"87654321";
+   signal s_wb_cyc_UutToSlave : std_logic;
+   signal s_wb_stb_UutToSlave : std_logic;
+   signal s_wb_ack_SlaveToUut : std_logic;
+   signal s_wb_ack_SlaveToUut_int : std_logic;
+   
+-- External trigger interface
+   signal s_ExtTrigger  : std_logic_vector(2**g_WB_Addr_Width - 1 downto 0) := (others => '0');
+   
+-- RTC value
+   signal s_RTCTime  : std_logic_vector(g_RTC_time_Width - 1 downto 0);
+   
+   
+   constant c_ClkPeriod : time := 20 ns;   -- 50 MHz
 
 begin
 
-   wb_rst_i <= '0' after 53 ns;
-   wb_clk_i <= not wb_clk_i after clk_period/2;
+   s_rst <= '0' after 53 ns;
+   s_clk <= not s_clk after c_ClkPeriod/2;
    
    process
    begin
       -- write read date
-      wait for 10*clk_period;
-      wb_we_i_User <= '1';
-      wb_adr_i_User <= "01" & "0" & "1011";
-      wb_dat_i_User <= X"00000003";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '1';
+      s_wb_adr_UserToUut <= "01" & "0" & "1011";
+      s_wb_dat_UserToUut <= X"00000003";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       -- write read recurrence
-      wait for 10*clk_period;
-      wb_we_i_User <= '1';
-      wb_adr_i_User <= "00" & "0" & "1011";
-      wb_dat_i_User <= X"00000010";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '1';
+      s_wb_adr_UserToUut <= "00" & "0" & "1011";
+      s_wb_dat_UserToUut <= X"00000010";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       
       -- write write date
-      wait for 10*clk_period;
-      wb_we_i_User <= '1';
-      wb_adr_i_User <= "01" & "1" & "1011";
-      wb_dat_i_User <= X"00000016";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '1';
+      s_wb_adr_UserToUut <= "01" & "1" & "1011";
+      s_wb_dat_UserToUut <= X"00000016";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       -- write write recurrence
-      wait for 10*clk_period;
-      wb_we_i_User <= '1';
-      wb_adr_i_User <= "00" & "1" & "1011";
-      wb_dat_i_User <= X"00000050";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '1';
+      s_wb_adr_UserToUut <= "00" & "1" & "1011";
+      s_wb_dat_UserToUut <= X"00000050";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       -- write write data
-      wait for 10*clk_period;
-      wb_we_i_User <= '1';
-      wb_adr_i_User <= "11" & "1" & "1011";
-      wb_dat_i_User <= X"12345678";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '1';
+      s_wb_adr_UserToUut <= "11" & "1" & "1011";
+      s_wb_dat_UserToUut <= X"12345678";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       
       wait for 1 ms;
       
       -- read read date
-      wait for 10*clk_period;
-      wb_we_i_User <= '0';
-      wb_adr_i_User <= "10" & "0" & "1011";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '0';
+      s_wb_adr_UserToUut <= "10" & "0" & "1011";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       -- read read data
-      wait for 10*clk_period;
-      wb_we_i_User <= '0';
-      wb_adr_i_User <= "11" & "0" & "1011";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '0';
+      s_wb_adr_UserToUut <= "11" & "0" & "1011";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       
       -- read write date
-      wait for 10*clk_period;
-      wb_we_i_User <= '0';
-      wb_adr_i_User <= "10" & "1" & "1011";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '0';
+      s_wb_adr_UserToUut <= "10" & "1" & "1011";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       -- read write data
-      wait for 10*clk_period;
-      wb_we_i_User <= '0';
-      wb_adr_i_User <= "11" & "1" & "1011";
-      wb_cyc_i_User <= '1';
-      wb_stb_i_User <= '1';
-      wait on wb_ack_o_User;
-      wait on wb_ack_o_User;
-      wb_we_i_User <= '0';
-      wb_cyc_i_User <= '0';
-      wb_stb_i_User <= '0';
+      wait for 10*c_ClkPeriod;
+      s_wb_we_UserToUut <= '0';
+      s_wb_adr_UserToUut <= "11" & "1" & "1011";
+      s_wb_cyc_UserToUut <= '1';
+      s_wb_stb_UserToUut <= '1';
+      wait on s_wb_ack_UutToUser;
+      wait on s_wb_ack_UutToUser;
+      s_wb_we_UserToUut <= '0';
+      s_wb_cyc_UserToUut <= '0';
+      s_wb_stb_UserToUut <= '0';
       
       wait;
    end process;
 
-   Master_Interface_Ack_process : process(wb_clk_i)
+   Master_Interface_Ack_process : process(s_rst, s_clk)
    begin
-      if rising_edge(wb_clk_i) then
-         if wb_rst_i = '1' then
-            wb_ack_i_Master_int <= '0';
-         else
-            wb_ack_i_Master_int <= '0';
-            if wb_cyc_o_Master = '1' and wb_stb_o_Master = '1' and wb_ack_i_Master_int = '0' then
-               wb_ack_i_Master_int <= '1';
-            end if;
+      if s_rst = '1' then
+         s_wb_ack_SlaveToUut_int <= '0';
+      elsif rising_edge(s_clk) then
+         s_wb_ack_SlaveToUut_int <= '0';
+         if s_wb_cyc_UutToSlave = '1' and s_wb_stb_UutToSlave = '1' and s_wb_ack_SlaveToUut_int = '0' then
+            s_wb_ack_SlaveToUut_int <= '1';
          end if;
       end if;
    end process;
-   wb_ack_i_Master <= wb_ack_i_Master_int;
+   s_wb_ack_SlaveToUut <= s_wb_ack_SlaveToUut_int;
    
    MailBox_tb : MailBox
    generic map
    (
       g_RTCClockPeriode => g_RTCClockPeriode,
-      WB_Addr_Width => WB_Addr_Width,
-      WB_Data_Width => WB_Data_Width,
-      RTC_time_Width => RTC_time_Width
+      WB_Addr_Width => g_WB_Addr_Width,
+      WB_Data_Width => g_WB_Data_Width,
+      RTC_time_Width => g_RTC_time_Width
    )
    port map
    (
-      wb_clk_i => wb_clk_i,
-      wb_rst_i => wb_rst_i,
-      wb_we_i_User => wb_we_i_User,
-      wb_adr_i_User => wb_adr_i_User,
-      wb_dat_i_User => wb_dat_i_User,
-      wb_dat_o_User => wb_dat_o_User,
-      wb_cyc_i_User => wb_cyc_i_User,
-      wb_stb_i_User => wb_stb_i_User,
-      wb_ack_o_User => wb_ack_o_User,
-      wb_dtr_o_User => wb_dtr_o_User,
-      wb_atr_o_User => wb_atr_o_User,
-      wb_we_o_Master => wb_we_o_Master,
-      wb_adr_o_Master => wb_adr_o_Master,
-      wb_dat_o_Master => wb_dat_o_Master,
-      wb_dat_i_Master => wb_dat_i_Master,
-      wb_cyc_o_Master => wb_cyc_o_Master,
-      wb_stb_o_Master => wb_stb_o_Master,
-      wb_ack_i_Master => wb_ack_i_Master
+      wb_clk_i          => s_clk,
+      wb_rst_i          => s_rst,
+      
+      wb_we_User_i      => s_wb_we_UserToUut,
+      wb_adr_User_i     => s_wb_adr_UserToUut,
+      wb_dat_User_i     => s_wb_dat_UserToUut,
+      wb_dat_User_o     => s_wb_dat_UutToUser,
+      wb_cyc_User_i     => s_wb_cyc_UserToUut,
+      wb_stb_User_i     => s_wb_stb_UserToUut,
+      wb_ack_User_o     => s_wb_ack_UutToUser,
+   
+      DataAvailable_o   => s_DataAvailable,
+      AddrToRead_o      => s_AddrToRead,
+      
+      wb_we_Master_o    => s_wb_we_UutToSlave,
+      wb_adr_Master_o   => s_wb_adr_UutToSlave,
+      wb_dat_Master_i   => s_wb_dat_UutToSlave,
+      wb_dat_Master_o   => s_wb_dat_SlaveToUut,
+      wb_cyc_Master_o   => s_wb_cyc_UutToSlave,
+      wb_stb_Master_o   => s_wb_stb_UutToSlave,
+      wb_ack_Master_i   => s_wb_ack_SlaveToUut,
+   
+   -- External trigger interface
+      ExtTrigger_i      => s_ExtTrigger,
+      
+   -- RTC value
+      RTCTime_o         => s_RTCTime
    );
 
 end behavior;
